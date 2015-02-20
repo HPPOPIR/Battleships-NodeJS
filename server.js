ceb4c5c40ebb881d66410cba8bc5aad107e292f3
OPIR <hristian_parushev@abv.bg>
2015-02-20 23:45:41 +0200

    asd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * Created by Todor Todorin on 2/20/2015.
  */
 var express  = require('express');
-var app      = express(); 								// create our app w/ express
+var app      = express(); 								// create our public w/ express
 var port  	 = process.env.PORT || 8080; 				// set the port
 
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
@@ -10,14 +10,14 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // configuration ===============================================================
 
-app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users									// log every request to the console
+app.use(express.static(__dirname)); 				// set the static files location /public/img will be /img for users									// log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
 // routes ======================================================================
-require('./routes.js')(app);
+require('./server/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
