6febcb211d7e883cf4f0a0fa9794b5c25e03349e
TodorTodorin <toti.1@abv.bg>
2015-02-21 01:04:21 +0200

    working on views

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
- angular.module('BattleShip', ['ngRoute'])
+angular.module('BattleShip', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when({redirectTo: '/single',templateUrl: 'public/main/single.html',
-            controller: 'SingleCtrl'});
-         $routeProvider.when({redirectTo: '/multi',templateUrl: 'public/multi/multi.html',
-             controller: 'MultiCtrl'});
-         $routeProvider.otherwise({redirectTo: '/main',templateUrl: 'public/main/main.html',
-            controller: 'MainCtrl'});
+        $routeProvider
+            .when('/single',{ templateUrl: 'public/main/single.html',
+            controller: 'SingleCtrl', reloadOnSearch: false})
+            .when('/multi',{ templateUrl: 'public/multi/multi.html',
+                controller: 'MultiCtrl', reloadOnSearch: false})
+            .otherwise({redirectTo: '/main', templateUrl: 'public/main/main.html',
+                controller: 'MainCtrl', reloadOnSearch: false});
     }]);
 
